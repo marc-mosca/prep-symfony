92f31ec5551312d28c2ccb52d46f06c0eec04c45
Marc MOSCA <marc.mosca@alesagglo.fr>
2026-01-16 15:37:13 +0100

    Suppression des commentaires inutiles

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,12 +9,6 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
 
-    /**
-     * Cette route correspond à la racine de l'application ("/"). Elle affiche la page d'accueil en rendant le template
-     * Twig `home/index.html.twig`.
-     *
-     * @return Response Réponse HTTP contenant le rendu de la page d'accueil.
-     */
     #[Route("/", name: "home.index")]
     public function index(): Response
     {
