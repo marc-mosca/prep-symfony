2128f6959a5f0cd9eb8e3c1c550c3501f149d595
Marc MOSCA <marc.mosca@alesagglo.fr>
2026-01-19 13:46:25 +0100

    Mise a jour de l'acces du controlleur `User`

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -2,25 +2,6 @@
 
 namespace App\Twig;
 
-use Twig\Attribute\AsTwigFunction;
-
 class AppExtension
 {
-
-    #[AsTwigFunction('icon', isSafe: ["html"])]
-    public function svgIcon(string $name, array $attributes = []): string
-    {
-        $html = "";
-
-        foreach ($attributes as $key => $value) {
-            $html .= sprintf(' %s="%s"', htmlspecialchars($key), htmlspecialchars($value));
-        }
-
-        return <<<HTML
-        <svg {$html}>
-          <use href="/sprite.svg?logo#{$name}"></use>
-        </svg>
-        HTML;
-    }
-
 }
