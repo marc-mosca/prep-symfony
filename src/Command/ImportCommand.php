0e01c1525fd7b76917ef3ce4dea613e037b04633
Marc MOSCA <marc.mosca@alesagglo.fr>
2026-01-23 11:44:51 +0100

    Suppression des import inutiles

--- src/Command/ImportCommand.php
+++ src/Command/ImportCommand.php
@@ -3,15 +3,12 @@
 namespace App\Command;
 
 use App\Command\Tools\UserImporter;
-use Doctrine\DBAL\Connection;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 #[AsCommand(
     name: 'app:import',
